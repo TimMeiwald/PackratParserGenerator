16cfa65ad3c37970a76163dad0156306e1ed3e5c
Tim Meiwald <tim.meiwald@gmail.com>
2022-08-13 14:17:58 +0100

    Can now automatically test that new parser output is identical to old parser output, will obviously break on grammar change

--- Generated_Output/test_generated_parser.py
+++ Generated_Output/test_generated_parser.py
@@ -3,38 +3,24 @@ from packratparsergenerator.parser.grammar_parser import Grammar_Parser as Gramm
 from os import getcwd
 from os.path import join
 from packratparsergenerator.parser.parser_pass_two import Parser_Pass_Two
-
+from time import time
 
 def test_generated_parser(parser_class, src):
     parser = parser_class()
     parser._set_src(src)
+    time_start = time()
     position, bool, node = parser._VAR_NAME(0, (parser.Grammar, None)) #var_name, Needed to ensure correct handling of deques
+    time_end = time()
+    print(f"{parser_class} time to parse is {time_end - time_start}")
     assert (len(src), True) == (position, True), "Parsing has failed"
     assert bool, "Parsing has failed"
     parsing = Parser_Pass_Two()
     parsing.parse(node)
-    node.pretty_print()
+    #node.pretty_print()
     return node
 
 
 if __name__ == "__main__":
-    """
-    path = join(getcwd(), "packratparsergenerator", "parser", "Grammar.txt")
-    with open(path, "r") as fp:
-        src = fp.read()
-    print(f"Length of File is : {len(src)}")
-    parser = Gen_Grammar_Parser()
-    parser._set_src(src)
-    position, bool, node = parser._VAR_NAME(0, (parser.Grammar, None)) #var_name, Needed to ensure correct handling of deques
-    print(position, bool)
-    assert (len(src), True) == (position, True), "Parsing has failed"
-    #node.pretty_print()
-    print(node)
-    parsing = Parser_Pass_Two()
-    parsing.parse(node)
-
-    node.pretty_print()
-    """
     path = join(getcwd(), "packratparsergenerator", "parser", "Grammar.txt")
     with open(path, "r") as fp:
         src = fp.read()
@@ -42,6 +28,6 @@ if __name__ == "__main__":
     old_parser = test_generated_parser(Grammar_Parser, src)
     new_parser = test_generated_parser(Gen_Grammar_Parser, src)
     if(new_parser == old_parser):
-        print("VICTORY")
+        print("Output of New Parser is Identical to Output of Old parser for given source input")
     else:
-        print("DOHHH")
+        print("DOHHH: Your New Parser does not have the same output as old parser, Is this expected? E.g Rule change? or Not")
