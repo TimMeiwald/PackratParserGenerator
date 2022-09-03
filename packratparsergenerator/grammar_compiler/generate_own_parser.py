b58295f00266dcce742fc661008250ff3c3c5c95
Tim Meiwald <tim.meiwald@gmail.com>
2022-09-03 20:22:11 +0100

    Cleaned some stuff up, added Generated_Output to gitignore

--- packratparsergenerator/grammar_compiler/generate_own_parser.py
+++ packratparsergenerator/grammar_compiler/generate_own_parser.py
@@ -2,8 +2,8 @@ if __name__ == "__main__":
     from packratparsergenerator.packratparsergenerator import PackratParserGenerator
     pgen = PackratParserGenerator()
     pgen.set_dest_filepath(
-        "C:\\Users\\timme\\Desktop\\MyPythonPackages\\PackratParserGenerator\\Generated_Output\\parser.py",
-        override=True)
+        "packratparsergenerator\\Generated_Output\\parser.py",
+        override=True, relative_path=True)
     pgen.set_src_filepath(
         "packratparsergenerator\\parser\\Grammar.txt",
         relative_path=True,
--- packratparsergenerator/grammar_compiler/testgrammartest.py
+++
@@ -1,8 +0,0 @@
-from Generated_Output.parser import Grammar_Parser, Parser
-from Generated_Output.parser import Parser_Pass_Two
-a = Grammar_Parser()
-
-
-b = a.parse("1234", a.int)
-print(b[0:1])
-b[2].pretty_print()
